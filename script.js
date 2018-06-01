7dc536b9f7c9251c599ac0adc2b00f802296cc12
laly <villxe0577@missioncisd.org>
2018-06-01 13:47:21 +0000

    final project deploy

---
+++ script.js
@@ -0,0 +1,5 @@
+let item1 = document.querySelector('#new')
+let item2 = document.querySelector('#popular')
+let item3 = document.querySelector('#all')
+
+item1.addEventListener('click', myfunction);
